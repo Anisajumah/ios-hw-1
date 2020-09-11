13ca417055f018f5dced957a237d739546098d73
Anisajumah <70814404+Anisajumah@users.noreply.github.com>
2020-09-11 23:57:07 +0300

    hw done

--- iOS-Hw1-Part1.playground/Contents.swift
+++ iOS-Hw1-Part1.playground/Contents.swift
@@ -7,7 +7,7 @@
 
 let name = "Ahmad"
 
-name = "Salem"
+var Name = "salem"
 
 
 var myWeight = 60
@@ -15,11 +15,11 @@ var FriendWeight = 80.5
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum = (myWeight) + Int(FriendWeight)
 
 
 
-var bmi = myWeight * (height * height)
+var bmi = Double(myWeight) * (height * height)
 
 /*
 - - - -
@@ -38,18 +38,26 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Anisa"
+var lastName1 = "Jumah"
+var email1 = "anisaj@gmail.com"
+var phoneNumber1 = "50556125"
+var age1 = 17
+var country1 = "kuwait"
+var area1 = "rawda"
+var password = "Anisa1234"
+var isKuwaiti = true
 
 //Person 2
-
+var firstname2 = "sara"
+var lastname2 = "alhindal"
+var email2 = "salhindal@gmail.com"
+var phonenumber2 = "99612303"
+var age2 = 18
+var country2 = "kuwait"
+var area2 = "rawda"
+var password2 = "sara2001"
+var iskuwaiti = true
 
 
 
@@ -60,6 +68,26 @@ var isKuwaiti = false
 
 
 //Person 3
+var firstname3 = "hamah"
+var lastname3 = "alanjari"
+var email3 = "alanjari2003@gmail.com"
+var phonenumber3 = "50571239"
+var age3 = 17
+var country3 = "kuwait"
+var area3 = "rawda"
+var iskuwaiti3 = true
+age1 == age3
+age1 != age2
+age2 != age3
+area1 == area2
+area2 == area3
+area1 == area3
+age1 < 18
+age2 == 18
+age3 < 18
+age1 == 17
+age2 != 17
+age3 == 17
 
 
 
@@ -79,6 +107,17 @@ var isKuwaiti = false
  * قارن اذا عمر الاشخاص اصغر من ١٨
  * قارن اذا عمر الاشخاص يساوي  ١٧
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
-
+age == age3
+ age != age2
+ age2 != age3
+ area1 == area2 == area3
+ age1 < 18
+ age2 => 18
+ age3 < 18
+ age1 == 17
+ age != 17
+ age3 == 17
+ 
+ 
  
  */
